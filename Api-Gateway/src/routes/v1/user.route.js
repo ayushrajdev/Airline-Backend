d19aa65730499d855c5d7876ebcf3d7d63e74e4c
ayushrajdev <ayushraj.dev10@gmail.com>
2026-06-13 10:48:22 +0530

    added authorization check for add role

--- Api-Gateway/src/routes/v1/user.route.js
+++ Api-Gateway/src/routes/v1/user.route.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const UserController = require('../../controllers/user.controller');
-const validateSignUpRequestBody = require('../../middlewares/user.middleware');
+const {
+    validateSignUpRequestBody,
+    isAdmin,
+    checkAuth,
+} = require('../../middlewares/user.middleware');
 const router = express.Router();
 
 const userController = new UserController();
 
 router.post('/sign-up', validateSignUpRequestBody, userController.create);
 router.post('/sign-in', userController.signin);
+router.post('/roles', checkAuth, isAdmin, userController.addRoleToUser);
 
 module.exports = router;
--- Api-Gateway/src/services/user.service.js
+++ Api-Gateway/src/services/user.service.js
@@ -86,6 +86,63 @@ class UserService extends CrudService {
             );
         }
     }
+
+    async addRoletoUser(data) {
+        try {
+            const user = await this.userRepository.get(data.id);
+            if (!user) {
+                throw new AppError(
+                    'No user found for the given id',
+                    StatusCodes.NOT_FOUND,
+                );
+            }
+            const role = await this.roleRepository.getRoleByName(data.role);
+            if (!role) {
+                throw new AppError(
+                    'No user found for the given role',
+                    StatusCodes.NOT_FOUND,
+                );
+            }
+            user.addRole(role);
+            return user;
+        } catch (error) {
+            if (error instanceof AppError) throw error;
+            console.log(error);
+            throw new AppError(
+                'Something went wrong',
+                StatusCodes.INTERNAL_SERVER_ERROR,
+            );
+        }
+    }
+
+   static async isAdmin(id) {
+        try {
+            const user = await userRepo.get(id);
+            if (!user) {
+                throw new AppError(
+                    'No user found for the given id',
+                    StatusCodes.NOT_FOUND,
+                );
+            }
+            const adminrole = await roleRepo.getRoleByName(
+                Enums.USER_ROLES_ENUMS.ADMIN,
+            );
+            if (!adminrole) {
+                throw new AppError(
+                    'No user found for the given role',
+                    StatusCodes.NOT_FOUND,
+                );
+            }
+            return user.hasRole(adminrole);
+        } catch (error) {
+            if (error instanceof AppError) throw error;
+            console.log(error);
+            throw new AppError(
+                'Something went wrong',
+                StatusCodes.INTERNAL_SERVER_ERROR,
+            );
+        }
+    }
 }
 
 module.exports = UserService;
